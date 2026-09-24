da3cec38fd94af8840a1dcf39865d249912d26d1
2022wc86041 <2022wc86041@wilp.bits-pilani.ac.in>
2026-09-24 21:46:46 +0530

    Modified HelloWorld message

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World from Jenkins and Maven!");
+        System.out.println("2022WC86041- Ashutosh Singh");
     }
 }
